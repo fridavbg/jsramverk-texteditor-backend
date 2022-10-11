1570c3d22d770aee0d0ef3dcbcc12eceec60ee7b
fridavbg <fperssontech@gmail.com>
2022-10-11 10:28:27 +0200

    Kmom06 GraphQL init

---
+++ graphql/root.js
@@ -0,0 +1,15 @@
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLList,
+    GraphQLInt,
+    GraphQLNonNull,
+} = require("graphql");
+
+const RootQueryType = new GraphQLObjectType({
+    name: "Query",
+    description: "Root Query",
+    fields: () => ({}),
+});
+
+module.exports = RootQueryType;
