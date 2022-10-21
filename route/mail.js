82baf6109f8d04c2207101eb11ea48cbe7b906c9
fridavbg <fperssontech@gmail.com>
2022-10-21 13:18:35 +0200

    Send mail post route fixed some typos

--- route/mail.js
+++ route/mail.js
@@ -12,14 +12,14 @@ router.get("/", (req, res) => {
 
 router.post(
     "/send",
-    // (req, res, next) => usersModel.checkToken(req, res, next),
+    (req, res, next) => usersModel.checkToken(req, res, next),
     async (req, res) => {
         const mailInput = req.body;
 
         const mail = await emailModel.sendEmail(mailInput);
 
-        // console.log("Mail confirmation:");
-        // console.log(mail);
+        console.log("Mail confirmation:");
+        console.log(mail);
 
         return res.json({
             msg: "Email has been sent",
